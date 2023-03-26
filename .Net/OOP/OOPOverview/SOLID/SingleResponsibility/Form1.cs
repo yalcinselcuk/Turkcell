4ae2d73575f0fcedbb878cb9ba89cf61996bc60c
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-03-27 00:00:10 +0300

    ProductService olusturuldu ve SRP'yi ihlal eden durum tespit edildi

--- .Net/OOP/OOPOverview/SOLID/SingleResponsibility/Form1.cs
+++ .Net/OOP/OOPOverview/SOLID/SingleResponsibility/Form1.cs
@@ -17,20 +17,41 @@ namespace SingleResponsibility
         //bir de turkay hoca yontemi var : bunu kod yazdıktan 
 
         //bu uygulamanin amaci db'ye urun eklemek
+        
         private void buttonAddProduct_Click(object sender, EventArgs e)
         {
-            SqlConnection sqlConnection = new SqlConnection("Data Source=LAPTOP-818NV7UV;Initial Catalog=deneme;Integrated Security=True");
-            SqlCommand sqlCommand = new SqlCommand("Insert into Products (ProductName, UnitPrice) values (@name, @price)", sqlConnection);
-            sqlCommand.Parameters.AddWithValue("@name", textBoxName.Text);
-            sqlCommand.Parameters.AddWithValue("@price", Convert.ToDecimal(textBoxPrice.Text));
+            // bir junior'in kodu :D
 
-            sqlConnection.Open();
-            var affectedRows = sqlCommand.ExecuteNonQuery();
-            sqlConnection.Close();
+            // SqlConnection, SqlCommand, Parametreler, Baglanti acma ve kapatma kismi da ProductService'in gorevi
+            
+            ProductService productService = new ProductService();
+            string name = textBoxName.Text;
+            decimal price = Convert.ToDecimal(textBoxPrice.Text);
+
+            var affectedRows = productService.AddProduct(name, price);
 
             string message = affectedRows > 0 ? "Basarili" : "İslem Yapilamadi";
             MessageBox.Show(message);
 
+            // bu formda degisiklik yapilmak istense ;
+            // 1.formun arayuzu, design'i : renk, yazi stili, controllerin yerleri
+            // 2.veritabaniyla iliskiniz degistiginde bu nesnede degisiklik yapilmasi gerekiyor
+            // yani su anda srp'yi ihlal ediyoruz 
+            
+            // sira Turkay Hoca'nin tekniginde 
+            // Form1 insan olsaydi gorevini nasil tanimlardi?
+            // derdi ki : benim gorevim kullanicidan verileri almak, butona tiklanmasini takip etmek, arkada olan sonucu kullaniciya gostermek 
+            // baska bir gorevim yok derdi
+
+            // o zaman diyoruz ki bu tanimin disinda yer alan her sey baska bir class'in gorevidir
+
+            // 2. Form1 insan evladi olsaydi, sorumlulugunu nasil anlatirdi ? (Turkay Hoca Yontemi)
+
+            //parametreleri atamak ve mesajla gostermek Form1'in gorevi 
+            //onun disindakiler baska bir elemanin gorevi
+            //o gorevler icin de yeni bir eleman almamiz lazim ve bu arkadas gorevi ustlenecek
+        
+            //ustteki kod Form1'in sorumlulugunu gerceklestirmekte
         }
     }
 }
---
+++ .Net/OOP/OOPOverview/SOLID/SingleResponsibility/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingleResponsibility
+{
+    //bu class geriye veri islemlerini calistiracak ve ekleme isini icraa edecek bir metod olmasi lazim
+    //geriye affectedRows'u dondurmesi lazim
+    public class ProductService
+    {
+        public int AddProduct(string name, decimal price)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=LAPTOP-818NV7UV;Initial Catalog=deneme;Integrated Security=True");
+            SqlCommand sqlCommand = new SqlCommand("Insert into Products (ProductName, UnitPrice) values (@name, @price)", sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@name", name);
+            sqlCommand.Parameters.AddWithValue("@price", price);
+            //buradaki parametreler de Form1'deki tool'larla direk islem yapmayacak
+
+            sqlConnection.Open();
+            var affectedRows = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            return affectedRows;
+        }
+
+        //burada da soyle bir sikinti var
+        //AddProduct isleminde yapilan islemleri Update'te tekrar edicez 
+        //Dont Repeat Yourself'e dusmus oluyoruz
+        //Bunun onune gecmek icin de bir BaseClass'a ihtiyacimiz var 
+        //Bunun adina da klasik ad olan "DataAccess" diyelim
+
+        public int UpdateProduct(string name, decimal price)
+        {
+            return 0;
+        }
+        public void SendInfoMailToProductOwner(string mail)
+        {
+
+        }
+
+    }
+}
