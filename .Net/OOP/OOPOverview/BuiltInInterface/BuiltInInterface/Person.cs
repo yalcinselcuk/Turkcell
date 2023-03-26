5eb97901e2f937f6fc471b144450d8f2f977dd7a
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-03-26 17:42:54 +0300

    interface

---
+++ .Net/OOP/OOPOverview/BuiltInInterface/BuiltInInterface/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltInInterface
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        //public Address Address{ get; set; }
+        //ustteki gibi bir baglilik siki bir bagdir
+        //bir nesneyle en dusuk bag interface ile yapilir
+        //asagidaki gibi
+        public IAddress Address { get; set; }
+    }
+
+    public interface IAddress
+    {
+        //Marker: İşaretçi, bir nesnenin "ne olduğunu" belirtmek için de kullanılabilir.
+        //Address'e herhangi bir ozellik kazandirmadigindan bu interface marker'dir
+    }
+    public class Address : IAddress
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+
+    public class HomeAddress : Address
+    {
+
+    }
+
+    public class WorkAddress : Address
+    {
+
+    }
+
+
+    public class Mouse
+    {
+        public IBattery Pil { get; set; }
+    }
+
+    public interface IBattery
+    {
+
+    }
+    public class Duracell : IBattery
+    {
+
+    }
+    public class Kodak : IBattery
+    {
+
+    }
+
+    public class PowerB : IBattery
+    {
+
+    }
+}
---
+++ .Net/OOP/OOPOverview/BuiltInInterface/BuiltInInterface/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltInInterface
+{
+    public class Product : IComparable<Product>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public int CompareTo(Product? other)
+        {
+            //biz de karsilastirma yapmak icin Price'i sectik 
+            if (Price > other?.Price)
+            {
+                return 1;
+            }
+            else if (Price < other?.Price)
+            {
+                return -1;
+
+            }
+            return 0;
+        }
+    }
+
+    public class ProductCollection : IEnumerable<Product>
+    {
+        private List<Product> products = new List<Product>();
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Clear()
+        {
+            products.Clear();
+        }
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            foreach (var product in products)
+            {
+                yield return product;// yield anahtarı
+            }
+        }
+
+        public void SortProducts()
+        {
+            products.Sort(); //IComparable 'yi implement etmesi gerektiğini yazdi yoksa bu metod calismiyor
+            //yani diyor ki ozelliklerden en az bir tanesini comparable etmesi gerekiyor
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
