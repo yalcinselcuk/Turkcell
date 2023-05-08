f949eda06b238b199cd24208d7dd966c56492859
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-08 16:03:24 +0300

    junior kodlama :)

---
+++ .Net/OOP/OOPOverview/SOLID/LiskovSubstitution/GeometryLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public class GeometryLibrary
+    {
+        public Rectangle RectangleFactory()
+        {
+            // sonradan bir sekilde Square dondurmeye karar verdiniz...
+            return null;
+        }
+    }
+
+    public class Rectangle
+    {
+        public virtual int Width{ get; set; }
+        public virtual int Height{ get; set; }
+
+        public int GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    //dikdortgen'de yukseklik ve genislik farklidir ama karede aynidir
+    //biz de gittik ustteki class'taki ozellikleri virtual olarak tanimladik sonra
+    public class Square : Rectangle 
+    {
+        public override int Height { get => base.Height; set { base.Height = value; base.Width = value; } }
+        public override int Width { get => base.Width; set { base.Height = value; base.Width = value; } }
+    }
+
+}
---
+++ .Net/OOP/OOPOverview/SOLID/LiskovSubstitution/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * 
+ * Liskov diyor ki :
+ * bir sınıf bir başkasına miras veriyorsa ; bu sınıflar BİRBİRLERİNİN YERİNE KULLANILABİLİR olmalı.
+ * birbirinin yerine kullanıldığında herhangi bir hata vermemeli
+ * 
+ * Bir sınıf miras aldığı sınıfın özelliklerini AYNEN KULLANMAK ZORUNDADIR
+ * 
+ * 
+ */
+
+using LiskovSubstitution;
+
+Rectangle rectangle= new Rectangle { Width=5,Height=4 };
+Console.WriteLine($"Rect : {rectangle.GetArea()}");
+Square square = new Square { Width = 10 };
+Console.WriteLine($"Square : {square.GetArea()}");
