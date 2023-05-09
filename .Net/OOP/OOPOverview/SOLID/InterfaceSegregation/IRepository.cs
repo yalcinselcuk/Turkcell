b7c2d866746a7399aa585c3e2bee2999a540ab78
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-09 15:09:41 +0300

    interface segregation hatası gösterildi

---
+++ .Net/OOP/OOPOverview/SOLID/InterfaceSegregation/IRepository.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    /*
+    
+    IRepository ; bir varligi bir veri deposu ile iliskilerini duzenler.
+    Kaynak veri deposu : Excel, Db, API gibi her sey olabilir
+
+     */
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        IEnumerable<T> GetItemsByName(string name);
+
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductRepository : IRepository<Product>
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetItemsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CategoryRepository : IRepository<Category>
+    {
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        //bu metodu kullanamayız burada, product'a ozgu
+        public IEnumerable<Category> GetItemsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ .Net/OOP/OOPOverview/SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * 
+ * bir class bir interface'i implement ediyorsa ; interface icindeki her seyi kullabiliyor olmalidir
+ * 
+ * gereksiz ve kullanilmayan metodlar; tek bir interface'e baglanmak zorunda degildir
+ * 
+ */
+
+
