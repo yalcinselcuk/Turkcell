36475e69dd9aa2e4606b752cac366ef9da32ccda
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-09 21:40:27 +0300

    2005 yılındaki .net 2.0 ile delegate

--- .Net/AdvancedFeatures/DelegatesAndEvents/usingDelegates/Program.cs
+++ .Net/AdvancedFeatures/DelegatesAndEvents/usingDelegates/Program.cs
@@ -4,7 +4,12 @@ List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 var helper = new FilterHelper();
 // .net 1.1, yıl olarak tahmini 2001-2003
 var filtered1 = helper.Filter(numbers,isEven);
+
+// .net 2.0 yıl olarak tahmini 2005
+var filtered2 = helper.Filter(numbers, delegate(int n) { return n % 2 == 1; });//bu kez de tek olanları dondurmesini istedik
 showNumbers(filtered1);
+showNumbers(filtered2);
+//biz delegate'e metod yazıp göndermiş olduk metod içinde
 
 
 
