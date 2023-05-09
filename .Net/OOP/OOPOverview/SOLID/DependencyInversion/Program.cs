3d051a30961e0e32b430f59274604a4a560646cb
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-09 20:07:03 +0300

    dependency inversion uygulandı

---
+++ .Net/OOP/OOPOverview/SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Dependency : bagimlilik, bir nesnenin varliginin baska bir nesneye bagli olmasi durumu
+ * 
+ * matkap ucu olmadan calismaz
+ * pil olmadan mouse calismaz
+ * ram olmadan pc calismaz
+ * 
+ * 
+ * Inversion : buyuk sistem, bagimlisi oldugu nesneyi olusturmamali. Disaridan almali
+ * 
+ */
+
+using DependencyInversion;
+
+Reporter reporter= new Reporter(new MailSender());
+reporter.SendReport();
+
+Reporter reporter2 = new Reporter(new WhatsappSender());
+reporter2.SendReport();
+
+Reporter reporter3 = new Reporter(new TelegramSender());
+reporter3.SendReport();
---
+++ .Net/OOP/OOPOverview/SOLID/DependencyInversion/Reporter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public class Reporter
+    {
+        private readonly ISender sender;
+        public Reporter(ISender sender) { this.sender = sender; }
+        public void SendReport()
+        {
+            //MailSender sender = new MailSender();
+            sender.Send();
+        }
+    }
+    public interface ISender
+    {
+        void Send();
+    }
+    public class MailSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Mail ile gonderildi");
+        }
+    }
+    public class WhatsappSender : ISender 
+    {
+        public void Send()
+        {
+            Console.WriteLine("Wp ile gonderildi");
+        }
+    }
+    public class TelegramSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("TelegramSender ile gonderildi");
+        }
+    }
+}
