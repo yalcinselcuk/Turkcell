ac94a89562d8c02badf8f352fb95ae6a467f2bdb
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-09 21:28:15 +0300

    ilk delegate oluşturuldu ve kullanıldı

---
+++ .Net/AdvancedFeatures/DelegatesAndEvents/usingDelegates/FilterHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingDelegates
+{
+    public class FilterHelper
+    {
+        //ilan verildi
+        public delegate bool criteria(int item);
+        public List<int> Filter(List<int> numbers, criteria aday)
+        {
+            List<int> filtered = new();
+            foreach (int number in numbers)
+            {
+                if (aday(number))
+                {
+                    filtered.Add(number);
+                }
+            }
+            return filtered;
+        }
+    }
+}
---
+++ .Net/AdvancedFeatures/DelegatesAndEvents/usingDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using usingDelegates;
+
+List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var helper = new FilterHelper();
+// .net 1.1, yıl olarak tahmini 2001-2003
+var filtered1 = helper.Filter(numbers,isEven);
+showNumbers(filtered1);
+
+
+
+
+void showNumbers(List<int> filtered1)
+{
+    foreach (int number in filtered1) 
+    {
+        Console.WriteLine(number);
+    }
+}
+
+bool isEven(int number)
+{
+    return number % 2 == 0;
+}
