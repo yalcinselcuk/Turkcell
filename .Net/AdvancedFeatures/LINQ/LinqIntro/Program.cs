34210686b819d514b3017f6785a3ffd7d4e1674f
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-10 22:07:19 +0300

    LINQ içinde extension metodun kullanıldığını gösteren bir örnek yapıldı

--- .Net/AdvancedFeatures/LINQ/LinqIntro/Program.cs
+++ .Net/AdvancedFeatures/LINQ/LinqIntro/Program.cs
@@ -14,5 +14,7 @@ var nameAndPriceList = from p in products
 
 nameAndPriceList.ToList().ForEach(x => Console.WriteLine($"{x.Ad   }\t{x.Fiyat}"));
 
-
+var sameResultWithExt = products.Where(p => p.Price < 1000) // where metodu bir extension metodudur
+                            .Select(p => new {Ad = p.Name, Fiyat = p.Price})
+                            .ToList();
 
