4e8476a6cefc766cbc17ddd31c932651e3d750b0
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-20 19:55:06 +0300

    Entity Framework Giriş ve Models

---
+++ .Net/AdvancedFeatures/introduceEFCore/introduceEFCore/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduceEFCore.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public IList<Book>? Books { get; set; }
+    }
+}
