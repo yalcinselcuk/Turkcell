37d059d9304193f211be5e544024cb641a3e9605
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-22 23:00:24 +0300

    DbFirst uygulandı

---
+++ EF CORE/DbFirst/DbFirst/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using DbFirst.Data;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+var context = new NorthwindContext().Products.ToList();
+context.ForEach(p => Console.WriteLine(p.ProductName));
