c6d721d4ba13746b30d4f29a279bca419562ffa5
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-24 11:38:29 +0300

    tek thread örneği

---
+++ .Net/AdvancedFeatures/MultiThreading/basicThread/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace basicThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCounter = new System.Windows.Forms.Button();
+            this.labelCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCounter
+            // 
+            this.buttonCounter.Location = new System.Drawing.Point(268, 125);
+            this.buttonCounter.Name = "buttonCounter";
+            this.buttonCounter.Size = new System.Drawing.Size(164, 35);
+            this.buttonCounter.TabIndex = 0;
+            this.buttonCounter.Text = "Sayacı Başlat";
+            this.buttonCounter.UseVisualStyleBackColor = true;
+            this.buttonCounter.Click += new System.EventHandler(this.buttonCounter_Click);
+            // 
+            // labelCounter
+            // 
+            this.labelCounter.AutoSize = true;
+            this.labelCounter.Location = new System.Drawing.Point(342, 195);
+            this.labelCounter.Name = "labelCounter";
+            this.labelCounter.Size = new System.Drawing.Size(0, 20);
+            this.labelCounter.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCounter);
+            this.Controls.Add(this.buttonCounter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonCounter;
+        private Label labelCounter;
+    }
+}
---
+++ .Net/AdvancedFeatures/MultiThreading/basicThread/Form1.cs
@@ -0,0 +1,24 @@
+﻿namespace basicThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCounter_Click(object sender, EventArgs e)
+        {
+            counter();//bu metod bitmeden aşağıdaki işlem yapılmaz : Senkron
+            MessageBox.Show("Bitti");
+        }
+        private void counter()
+        {
+
+            for (int i = 0; i < 50000; i++)
+            {
+                labelCounter.Text = i.ToString();
+            }
+        }
+    }
+}
