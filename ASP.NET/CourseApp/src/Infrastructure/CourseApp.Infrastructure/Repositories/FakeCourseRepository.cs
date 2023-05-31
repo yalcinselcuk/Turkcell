afcb03799a22fe2f6ebfe43a703bfc5a23b46972
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-31 21:25:51 +0300

    ortak bir view oluşturuldu ve veriler row'lara ayrıldı

--- ASP.NET/CourseApp/src/Infrastructure/CourseApp.Infrastructure/Repositories/FakeCourseRepository.cs
+++ ASP.NET/CourseApp/src/Infrastructure/CourseApp.Infrastructure/Repositories/FakeCourseRepository.cs
@@ -16,8 +16,28 @@ namespace CourseApp.Infrastructure.Repositories
             {
                 new Course {Id=1, Name="Course 1", Description="Description 1", Price=1, Rating=1, TotalHours=10},
                 new Course {Id=2, Name="Course 2", Description="Description 2", Price=2, Rating=2, TotalHours=20},
-                new Course {Id=1, Name="Course 3", Description="Description 3", Price=3, Rating=3, TotalHours=30},
-                new Course {Id=1, Name="Course 4", Description="Description 4", Price=4, Rating=4, TotalHours=40}
+                new Course {Id=3, Name="Course 3", Description="Description 3", Price=3, Rating=3, TotalHours=30},
+                new Course {Id=4, Name="Course 4", Description="Description 4", Price=4, Rating=4, TotalHours=40},
+                new Course {Id=5, Name="Course 5", Description="Description 1", Price=1, Rating=1, TotalHours=10},
+                new Course {Id=6, Name="Course 6", Description="Description 2", Price=2, Rating=2, TotalHours=20},
+                new Course {Id=7, Name="Course 7", Description="Description 3", Price=3, Rating=3, TotalHours=30},
+                new Course {Id=8, Name="Course 8", Description="Description 4", Price=4, Rating=4, TotalHours=40}, 
+                new Course {Id=9, Name="Course 9", Description="Description 1", Price=1, Rating=1, TotalHours=10},
+                new Course {Id=10, Name="Course 10", Description="Description 2", Price=2, Rating=2, TotalHours=20},
+                new Course {Id=11, Name="Course 11", Description="Description 3", Price=3, Rating=3, TotalHours=30},
+                new Course {Id=12, Name="Course 12", Description="Description 4", Price=4, Rating=4, TotalHours=40},
+                new Course {Id=13, Name="Course 13", Description="Description 1", Price=1, Rating=1, TotalHours=10},
+                new Course {Id=14, Name="Course 14", Description="Description 2", Price=2, Rating=2, TotalHours=20},
+                new Course {Id=15, Name="Course 15", Description="Description 3", Price=3, Rating=3, TotalHours=30},
+                new Course {Id=16, Name="Course 16", Description="Description 4", Price=4, Rating=4, TotalHours=40},
+                new Course {Id=17, Name="Course 17", Description="Description 1", Price=1, Rating=1, TotalHours=10},
+                new Course {Id=18, Name="Course 18", Description="Description 2", Price=2, Rating=2, TotalHours=20},
+                new Course {Id=19, Name="Course 19", Description="Description 3", Price=3, Rating=3, TotalHours=30},
+                new Course {Id=20, Name="Course 20", Description="Description 4", Price=4, Rating=4, TotalHours=40},
+                new Course {Id=21, Name="Course 21", Description="Description 1", Price=1, Rating=1, TotalHours=10},
+                new Course {Id=22, Name="Course 22", Description="Description 2", Price=2, Rating=2, TotalHours=20},
+                new Course {Id=23, Name="Course 23", Description="Description 3", Price=3, Rating=3, TotalHours=30},
+                new Course {Id=24, Name="Course 24", Description="Description 4", Price=4, Rating=4, TotalHours=40}
 
 
             };
