a213ad1b2d943658df6f20144939a32c5e10c57b
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-31 23:43:59 +0300

    dinamik pagination

--- ASP.NET/CourseApp/src/webUI/CourseApp.Mvc/Controllers/HomeController.cs
+++ ASP.NET/CourseApp/src/webUI/CourseApp.Mvc/Controllers/HomeController.cs
@@ -15,10 +15,32 @@ namespace CourseApp.Mvc.Controllers
             _courseService = courseService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNo = 1)
         {
             var courses = _courseService.GetCourseDisplayResponse();
-            return View(courses);
+            /*
+                1.sayfa : 0 eleman atla, 8 eleman al
+                2.sayfa : 8 eleman atla, 8 eleman al 
+                3.sayfa = 16 eleman atla, 8 eleman al
+             */
+
+            /*
+             Kursların toplam sayfa sayısı için gerekli bilgiler ;
+                -sayfada kaç kurs olacak
+                -toplam kaç kurs var
+             */
+
+            var coursePerPage = 8;
+            var courseCount = courses.Count();
+            var totalPage = Math.Ceiling((decimal)courseCount / coursePerPage);//yukarıya tamamladık 105 yerine 106 olursa, her sayfaya 5 tane olursa diye eksik sayfa olmasın
+            ViewBag.TotalPage = totalPage;
+
+            var paginatedCourses = courses.OrderBy(c => c.Id)
+                                          .Skip((pageNo-1)*coursePerPage)
+                                          .Take(coursePerPage)
+                                          .ToList();
+
+            return View(paginatedCourses);
         }
 
         public IActionResult Privacy()
