c39db9a61b81139782cac4e3378da93b5dde51b0
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-06-04 19:00:00 +0300

    identity Scaffolding Template

---
+++ ASP.NET/ScaffoldIdentity/ScaffoldIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScaffoldIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
