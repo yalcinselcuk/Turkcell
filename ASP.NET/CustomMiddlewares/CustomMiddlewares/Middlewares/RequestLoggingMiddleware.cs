eced8aea3700f9f237cacee085e32ed3bbbb030a
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-06-24 19:07:09 +0300

    gelen tüm request'leri kontrol etmek için middleware oluşturduk

---
+++ ASP.NET/CustomMiddlewares/CustomMiddlewares/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomMiddlewares.Middlewares
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;//kendisinden sonra nereye gideceği
+        private readonly ILogger<RequestLoggingMiddleware> _logger;//loglamak için ve bu klasöre özgü oluşturduk
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext  httpContext)//middleware çalıştığında bu fonksiyon çalışır
+        {
+            /*
+             * Bu middleware'in amacı:  api'ye gelen her request'i yakalamak ve bilgi vermektir
+             */
+            _logger.LogInformation($"Gelen request metodu : {httpContext.Request.Method}. Talebin gönderildiği adres : {httpContext.Request.Path}");
+            await _next(httpContext);//middleware'i gönderdik
+        }
+    }
+}
--- ASP.NET/CustomMiddlewares/CustomMiddlewares/Program.cs
+++ ASP.NET/CustomMiddlewares/CustomMiddlewares/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using CustomMiddlewares.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -9,41 +11,47 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+using var loggerFactory = LoggerFactory.Create(builder => builder.AddSimpleConsole());//middleware içinde logger aldığımızdan instance'ını böyle oluşturduk
+var logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
+//bu iki satırın amacı middleware içindeki ILogger'ı döndürmek
+
 var app = builder.Build();
 
-app.Use(async(context, next) =>
-{
-    await context.Response.WriteAsync("---");
-    await next();//bir sonraki middleware'e gönderdik, yani aşağıya
-    await context.Response.WriteAsync("---");//bir daha buraya dönüyorsa bunu döndürecek
-});
+//app.Use(async(context, next) =>
+//{
+//    await context.Response.WriteAsync("---");
+//    await next();//bir sonraki middleware'e gönderdik, yani aşağıya
+//    await context.Response.WriteAsync("---");//bir daha buraya dönüyorsa bunu döndürecek
+//});
 
-app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync(">>>");
-    await next();//bir sonraki middleware'e gönderdik
-    await context.Response.WriteAsync("<<<");//bir daha buraya dönüyorsa bunu döndürecek
-});
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync(">>>");
+//    await next();//bir sonraki middleware'e gönderdik
+//    await context.Response.WriteAsync("<<<");//bir daha buraya dönüyorsa bunu döndürecek
+//});
 
 //Http'den gelen bütün request'leri yakalaması için temsilen "context" dedik
 //her yapılan işlem bşr middleware olduğundan context'ten sonra bir sonraki middleware'e göndermemiz gerekir
 //onu da temsilen "next" diyoruz
 
+app.UseMiddleware<RequestLoggingMiddleware>();
+
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-app.Run(async context =>
+if (app.Environment.IsDevelopment())
 {
-    await context.Response.WriteAsync("Bunu middleware yazdı");
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//app.Run(async context =>
+//{
+//    await context.Response.WriteAsync("Bunu middleware yazdı");
+//});
 app.Run();
