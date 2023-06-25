77f3508f00b215c4d4e5127395070697124719b0
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-06-25 15:58:24 +0300

    bu middleware içinde bütün request'leri yakalayayacak olan kod parçasını ekledik

--- ASP.NET/CustomMiddlewares/CustomMiddlewares/Middlewares/RequestLoggingMiddleware.cs
+++ ASP.NET/CustomMiddlewares/CustomMiddlewares/Middlewares/RequestLoggingMiddleware.cs
@@ -16,8 +16,19 @@
             /*
              * Bu middleware'in amacı:  api'ye gelen her request'i yakalamak ve bilgi vermektir
              */
-            _logger.LogInformation($"Gelen request metodu : {httpContext.Request.Method}. Talebin gönderildiği adres : {httpContext.Request.Path}");
-            await _next(httpContext);//middleware'i gönderdik
+            _logger.LogInformation($"{DateTime.Now} anında Gelen request metodu : {httpContext.Request.Method}. Talebin gönderildiği adres : {httpContext.Request.Path}");
+            var responseBodyStream = httpContext.Response.Body; //üretilen çıktının sonucunu almak istiyoruz
+            var responseStream = new MemoryStream();//bellekte bir alan oluşturduk
+            httpContext.Response.Body = responseStream;//oluşturulan alanı aktar, var olan Body'i responseStream'le değiştir
+            await _next(httpContext);//middleware'i gönderdik, response'u döndürdü büyük ihtimal
+            responseStream.Seek(0, SeekOrigin.Begin);//başlangıç pozisyonuna dönücez
+            var responseBody = new StreamReader(responseStream).ReadToEnd();//belirttiğimiz responseStream'i baştan sona oku
+            _logger.LogInformation($"{DateTime.Now} anında  Response oluştu...");
+            _logger.LogInformation($"Oluşan response : {httpContext.Response.StatusCode} -> {responseBody}");
+
+            responseStream.Seek(0, SeekOrigin.Begin);
+            await responseStream.CopyToAsync(responseBodyStream);
+
         }
     }
 }
